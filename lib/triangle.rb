a7036dc72869400864be739f675a190a555bdda0
Daniel J Feleppa <dfeleppa@gmail.com>
2020-03-01 22:04:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,37 +2,40 @@ class Triangle
   attr_accessor :side_one, :side_two, :side_three
   
   def initialize(side_one, side_two, side_three)
-    
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
   end
   
-  
+  def kind
   
 end
-
-
-class Person
-  attr_accessor :partner, :name
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def get_married(person)
-    self.partner = person
-    if person.class != Person
-      begin
-        raise PartnerError
-      rescue PartnerError => error
-          puts error.message
-      end
-    else
-      person.partner = self
-    end
-  end
- 
-  class PartnerError < StandardError
+  
+  
+  
+  class TriangleError < StandardError
     def message
       "you must give the get_married method an argument of an instance of the person class!"
     end
   end
+  
+  
 end
+
+
+
+ 
+ # def get_married(person)
+  #  self.partner = person
+   # if person.class != Person
+    #  begin
+     #   raise PartnerError
+      
+      #rescue PartnerError => error
+       #   puts error.message
+      #end
+  #  else
+   #   person.partner = self
+   # end
+  #end
+ 
